5348ef8267befb08b7a15a96669bfb2a805fccdc
MaksimK <maksimka_k-ov@outlook.com>
2021-10-12 21:23:31 +0300

    Второй пункт пятого задания

--- homework_04_firstpage/04_firstpage.js
+++ homework_04_firstpage/04_firstpage.js
@@ -6,3 +6,14 @@ alert('true')
 }
 else { alert('false')
 }
+
+
+function truncate(str, maxlength) {
+    if (str.length > maxlength) {
+        str = str.substring(0, maxlength);
+        return str + "...";
+    }
+    else
+    return str;
+}
+alert(truncate("Функция усекает строку до количества символов и добавляет многоточие.", 32));
