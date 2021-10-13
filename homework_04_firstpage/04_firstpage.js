7c76437ca7ca453737158bbc075a002b5aca42cc
MaksimK <maksimka_k-ov@outlook.com>
2021-10-13 19:33:38 +0300

    Третий пункт пятого задания

--- homework_04_firstpage/04_firstpage.js
+++ homework_04_firstpage/04_firstpage.js
@@ -1,5 +1,5 @@
-const firstStr = prompt ()
-const secondStr = prompt ()
+let firstStr = prompt ()
+let secondStr = prompt ()
 
 if(firstStr == secondStr) {
 alert('true')
@@ -17,3 +17,14 @@ function truncate(str, maxlength) {
     return str;
 }
 alert(truncate("Функция усекает строку до количества символов и добавляет многоточие.", 32));
+
+const secondRegExp = new RegExp('^\\d\\d[/]\\d\\d[/]\\d\\d\\d\\d \\d-\\d$');
+const regexp = /^\d\d[/]\d\d[/]\d\d\d\d \d\d-\d\d$/;
+
+const firstString = '12/02/2021 12-00';
+
+alert((firstString.replace('/', '.').replace('/', '.').replace('-', ':')));
+
+
+
+
