68011a9eab8d9949df553b2919d590b6d7e3d99e
MaksimK <maksimka_k-ov@outlook.com>
2021-10-21 19:06:44 +0300

    Второй пункт

--- homework_05_firstpage/05_firstpage.js
+++ homework_05_firstpage/05_firstpage.js
@@ -4,3 +4,12 @@ const arr = [1, 2, 3, 4, 5, 6, 2, 3, 4, 6];
 console.log(arr)
 const secondArr = new Set(arr);
 console.log(secondArr);
+
+// Второе задание
+const thirdArr = [1, 2, 3, 4, 5, 6];
+const reverseArr = thirdArr.reduceRight ((accumulator, value) => {
+accumulator.push (value);
+return accumulator;
+}, []);
+console.log(thirdArr);
+console.log(reverseArr);
