5f9bf8fa44b3702c9048aa320f8a91bc34f1d7c6
MaksimK <maksimka_k-ov@outlook.com>
2021-10-21 19:07:36 +0300

    Четвертый пункт

--- homework_05_firstpage/05_firstpage.js
+++ homework_05_firstpage/05_firstpage.js
@@ -19,3 +19,20 @@ const fourthArr = [['Год', '2021'], ['Век', 'XXI']];
 console.log(fourthArr);
 const obj = Object.fromEntries(fourthArr);
 console.log(obj);
+
+// Четвертое задание
+const secondObj = {
+  Москва: 12655050,
+  Берлин: 3644826,
+  Вашингтон: 705749
+}
+function sum(secondObj) {
+  let sum = 0;
+  for (let key in secondObj) {
+    if (typeof secondObj[key] == 'number') {
+       sum += secondObj[key];
+    }
+  }
+  return sum;
+}
+console.log(sum(secondObj));
