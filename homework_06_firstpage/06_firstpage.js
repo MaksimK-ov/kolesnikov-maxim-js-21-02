c06dd8281a63d685c83e42d5b5a96f806cda280c
MaksimK <maksimka_k-ov@outlook.com>
2021-10-25 19:28:14 +0300

    Второе задание

--- homework_06_firstpage/06_firstpage.js
+++ homework_06_firstpage/06_firstpage.js
@@ -8,3 +8,29 @@ return Fib (n - 1) + Fib(n - 2);
 }
 }
 console.log(Fib(40));
+
+// Второе задание
+function Fib  (n)  {
+if (n <= 1) {
+return n;
+}
+else {
+return Fib (n - 1) + Fib(n - 2);
+}
+}
+const memoizeFib = () => {
+  const cache = {};
+  return (n) => {
+    if (n in cache) {
+      return cache[n];
+    }
+    else {
+      const result = Fib (n - 1) + Fib(n - 2);
+      cache[n] = result;
+      return result;
+    }
+  }
+}
+const newFib = memoizeFib();
+console.log(Fib(40));
+console.log(newFib(41));
